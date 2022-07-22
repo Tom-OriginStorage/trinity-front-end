bd813aee0b496ca464a93b2fbacb1ca811bfcb29
Tom-OriginStorage <jefferson.chu@chainup.com>
2022-07-22 16:41:47 +0800

    Add video

--- src/pages/Records.tsx
+++ src/pages/Records.tsx
@@ -5,9 +5,7 @@ import { ethers } from "ethers";
 export default function Records() {
 
   return (
-    <div>
-
-    </div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/RaXr9sdPb8Q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   )
 
 }
