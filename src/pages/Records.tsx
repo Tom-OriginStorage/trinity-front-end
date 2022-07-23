b7d3738ed2c1924a425cf37fe3eff7b2249262c8
Tom-OriginStorage <103015469+Tom-OriginStorage@users.noreply.github.com>
2022-07-23 20:56:18 +0800

    Update Records.tsx

--- src/pages/Records.tsx
+++ src/pages/Records.tsx
@@ -5,7 +5,7 @@ import { ethers } from "ethers";
 export default function Records() {
 
   return (
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/RaXr9sdPb8Q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/ZByCaZFm814" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
   )
 
 }
